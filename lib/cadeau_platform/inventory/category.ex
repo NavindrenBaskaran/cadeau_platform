360bfd352683f985f56b0589b69bfb4148055409
Navindren <navin25@hotmail.com>
2018-02-10 22:17:41 +0800

    Add category type enums to category

--- lib/cadeau_platform/inventory/category.ex
+++ lib/cadeau_platform/inventory/category.ex
@@ -2,11 +2,13 @@ defmodule CadeauPlatform.Inventory.Category do
   use Ecto.Schema
   import Ecto.Changeset
   alias CadeauPlatform.Inventory.{Category, Product}
+  import EctoEnum
 
+  defenum CategoryTypeEnum, book: 0, medical: 1, food: 2, wear: 3
 
   schema "categories" do
     field :name, :string
-    field :type, :integer
+    field :type, CategoryTypeEnum
     has_many :products, Product
     timestamps()
   end
