3f8d9a1da01ef1c7586362fa01949dab5f4ab770
Navindren <navin25@hotmail.com>
2018-02-10 18:55:38 +0800

    Add money library

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule CadeauPlatform.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:comeonin, "~> 2.5"}
+      {:comeonin, "~> 2.5"},
+      {:money, "~> 1.2.1"}
     ]
   end
 
