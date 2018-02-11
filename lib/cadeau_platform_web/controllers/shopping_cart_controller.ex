a1d9da6f7eb016f0084f35972e6645860f2f3eb8
Navindren <navin25@hotmail.com>
2018-02-11 16:47:54 +0800

    Add shopping_cart controller and view

---
+++ lib/cadeau_platform_web/controllers/shopping_cart_controller.ex
@@ -0,0 +1,10 @@
+defmodule CadeauPlatformWeb.ShoppingCartController do
+  use CadeauPlatformWeb, :controller
+
+  alias CadeauPlatform.Inventory
+
+  def index(selected_items) do
+    
+  end
+
+end
