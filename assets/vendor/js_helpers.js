632971010c53fce2eb85e0d48438f390e7810799
Navindren <navin25@hotmail.com>
2018-02-11 15:15:01 +0800

    Set selected products by user as cookies

--- assets/vendor/js_helpers.js
+++ assets/vendor/js_helpers.js
@@ -1,3 +1,21 @@
 function addToShoppingCart(product_id){
-  console.log(product_id)
+  var expires = "";
+  var shopping_cart_cookie = getShoppingCartCookies();
+  var date = new Date();
+
+  date.setTime(date.getTime() + (2*24*60*60*1000))
+  expires = "; expires=" + date.toUTCString();
+
+  if (shopping_cart_cookie){
+    document.cookie = shopping_cart_cookie + "," + ( product_id || "")  + expires + "; path=/";
+
+  }else{
+    document.cookie = "items" + "=" + ( product_id || "")  + expires + "; path=/";
+  }
+}
+
+function getShoppingCartCookies(){
+  var cookies_list = document.cookie.split(";")
+  var shopping_cart_cookie = cookies_list.pop()
+  return shopping_cart_cookie
 }
