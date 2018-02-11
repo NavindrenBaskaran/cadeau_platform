1fa4f23648237e4d00b63c1688c589f63836581d
Navindren <navin25@hotmail.com>
2018-02-11 11:03:38 +0800

    Add view helper to decide imported or local product

--- lib/cadeau_platform_web/views/page_view.ex
+++ lib/cadeau_platform_web/views/page_view.ex
@@ -1,3 +1,12 @@
 defmodule CadeauPlatformWeb.PageView do
   use CadeauPlatformWeb, :view
+
+  def imported_product?(item) do
+    case item.imported do
+      true
+        -> "Foreign Product"
+      false
+        -> "Local Product"
+    end
+  end
 end
