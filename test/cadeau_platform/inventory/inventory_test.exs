ad179aa5371794b51a0dd8c8b6fc1b042a5cddea
Navindren <navin25@hotmail.com>
2018-02-13 06:09:29 +0800

    Auto generated test file, will be removing it later

--- test/cadeau_platform/inventory/inventory_test.exs
+++ test/cadeau_platform/inventory/inventory_test.exs
@@ -118,4 +118,120 @@ defmodule CadeauPlatform.InventoryTest do
       assert %Ecto.Changeset{} = Inventory.change_category(category)
     end
   end
+
+  describe "taxes" do
+    alias CadeauPlatform.Inventory.Tax
+
+    @valid_attrs %{}
+    @update_attrs %{}
+    @invalid_attrs %{}
+
+    def tax_fixture(attrs \\ %{}) do
+      {:ok, tax} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Inventory.create_tax()
+
+      tax
+    end
+
+    test "list_taxes/0 returns all taxes" do
+      tax = tax_fixture()
+      assert Inventory.list_taxes() == [tax]
+    end
+
+    test "get_tax!/1 returns the tax with given id" do
+      tax = tax_fixture()
+      assert Inventory.get_tax!(tax.id) == tax
+    end
+
+    test "create_tax/1 with valid data creates a tax" do
+      assert {:ok, %Tax{} = tax} = Inventory.create_tax(@valid_attrs)
+    end
+
+    test "create_tax/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Inventory.create_tax(@invalid_attrs)
+    end
+
+    test "update_tax/2 with valid data updates the tax" do
+      tax = tax_fixture()
+      assert {:ok, tax} = Inventory.update_tax(tax, @update_attrs)
+      assert %Tax{} = tax
+    end
+
+    test "update_tax/2 with invalid data returns error changeset" do
+      tax = tax_fixture()
+      assert {:error, %Ecto.Changeset{}} = Inventory.update_tax(tax, @invalid_attrs)
+      assert tax == Inventory.get_tax!(tax.id)
+    end
+
+    test "delete_tax/1 deletes the tax" do
+      tax = tax_fixture()
+      assert {:ok, %Tax{}} = Inventory.delete_tax(tax)
+      assert_raise Ecto.NoResultsError, fn -> Inventory.get_tax!(tax.id) end
+    end
+
+    test "change_tax/1 returns a tax changeset" do
+      tax = tax_fixture()
+      assert %Ecto.Changeset{} = Inventory.change_tax(tax)
+    end
+  end
+
+  describe "products_taxes" do
+    alias CadeauPlatform.Inventory.ProductTax
+
+    @valid_attrs %{}
+    @update_attrs %{}
+    @invalid_attrs %{}
+
+    def product_tax_fixture(attrs \\ %{}) do
+      {:ok, product_tax} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Inventory.create_product_tax()
+
+      product_tax
+    end
+
+    test "list_products_taxes/0 returns all products_taxes" do
+      product_tax = product_tax_fixture()
+      assert Inventory.list_products_taxes() == [product_tax]
+    end
+
+    test "get_product_tax!/1 returns the product_tax with given id" do
+      product_tax = product_tax_fixture()
+      assert Inventory.get_product_tax!(product_tax.id) == product_tax
+    end
+
+    test "create_product_tax/1 with valid data creates a product_tax" do
+      assert {:ok, %ProductTax{} = product_tax} = Inventory.create_product_tax(@valid_attrs)
+    end
+
+    test "create_product_tax/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Inventory.create_product_tax(@invalid_attrs)
+    end
+
+    test "update_product_tax/2 with valid data updates the product_tax" do
+      product_tax = product_tax_fixture()
+      assert {:ok, product_tax} = Inventory.update_product_tax(product_tax, @update_attrs)
+      assert %ProductTax{} = product_tax
+    end
+
+    test "update_product_tax/2 with invalid data returns error changeset" do
+      product_tax = product_tax_fixture()
+      assert {:error, %Ecto.Changeset{}} = Inventory.update_product_tax(product_tax, @invalid_attrs)
+      assert product_tax == Inventory.get_product_tax!(product_tax.id)
+    end
+
+    test "delete_product_tax/1 deletes the product_tax" do
+      product_tax = product_tax_fixture()
+      assert {:ok, %ProductTax{}} = Inventory.delete_product_tax(product_tax)
+      assert_raise Ecto.NoResultsError, fn -> Inventory.get_product_tax!(product_tax.id) end
+    end
+
+    test "change_product_tax/1 returns a product_tax changeset" do
+      product_tax = product_tax_fixture()
+      assert %Ecto.Changeset{} = Inventory.change_product_tax(product_tax)
+    end
+  end
 end
