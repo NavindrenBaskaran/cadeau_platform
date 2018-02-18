841f6c8e4dd015511eb0f3308ce45a11ceb5a3e4
Navindren <navin25@hotmail.com>
2018-02-18 14:24:21 +0800

    Remove unrequired functions, add error message

--- assets/vendor/js_helpers.js
+++ assets/vendor/js_helpers.js
@@ -1,45 +1,3 @@
-function addToShoppingCart(product_id){
-  var expires = "";
-  var shopping_cart_cookie = getShoppingCartCookies();
-  var date = new Date();
-
-  date.setTime(date.getTime() + (2*24*60*60*1000))
-  expires = "; expires=" + date.toUTCString();
-
-  if (shopping_cart_cookie){
-    document.cookie = shopping_cart_cookie + ":" + ( product_id || "")  + expires + "; path=/";
-  }else{
-    document.cookie = "items" + "=" + ( product_id || "")  + expires + "; path=/";
-  }
-  updateselectedItemsCount()
-}
-
-function updateselectedItemsCount(){
-  var $itemCount = $("#itemCount");
-  if (selectedItems()) {
-    $itemCount.html(selectedItems().length);
-  }else{
-    $itemCount.html(0);
-  }
-}
-
-function selectedItems(){
-  var $itemCount = $("#itemCount");
-  var shopping_cart_cookie = getShoppingCartCookies();
-  if (shopping_cart_cookie){
-    var items = shopping_cart_cookie.split("=")[1].split(":")
-    return Array.from(new Set(items))
-  }else{
-    []
-  }
-}
-
-function getShoppingCartCookies(){
-  var cookies_list = document.cookie.split(";")
-  var shopping_cart_cookie = cookies_list.pop()
-  return shopping_cart_cookie
-}
-
 function addItemToCart(product_id){
   var csrf = $('input[name=csrf_token]')[0].value
   $.ajax({
@@ -51,7 +9,7 @@ function addItemToCart(product_id){
     if (msg.status == 200){
       alert("Item Added");
     }else{
-      alert("Failed to Add Item")
+      alert("Should Login First.")
     }
   });
 }
